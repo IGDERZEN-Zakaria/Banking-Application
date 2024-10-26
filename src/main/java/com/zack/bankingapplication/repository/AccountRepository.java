7302310be6d9fdc0b8cf497a7c06f44060a2e1b8
Zack <zakaria.igderzen@gmail.com>
2024-10-26 19:55:13 +0200

    Adding Repositories

---
+++ src/main/java/com/zack/bankingapplication/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.zack.bankingapplication.repository;
+
+import com.zack.bankingapplication.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
